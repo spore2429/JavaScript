077770431e3dede29d8f3615a1067edbbf4cc46d
spore2429 <spore2429@naver.com>
2023-12-16 17:55:34 +0900

    12-16

---
+++ htmlwork/js/pop.js
@@ -0,0 +1,8 @@
+function popup1(){
+
+    window.open('../div_img/beauty1.jpg','w','width=400,height=500') //파일,이름,상태(a,b,c)
+}
+
+function naver1(){
+    window.open('https://m.naver.com/','NAVER','width=500,height=500')
+}
---
+++ htmlwork/js/rndColor.js
@@ -0,0 +1,8 @@
+function rndColor(){
+    //rgb색상얻기
+    var r=parseInt(Math.random()*256);//0~255
+    var g=parseInt(Math.random()*256);//0~255
+    var b=parseInt(Math.random()*256);//0~255
+    var color="rgb("+r+","+g+","+b+")";
+    return color;
+}
